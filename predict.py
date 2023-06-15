2ff246b7f0717ca9de2b1f92d8a88f6c9a4a9371
Kim-Junseop <wnqtjq8958@naver.com>
2023-06-15 15:36:55 +0900

    modify predict.py

--- predict.py
+++ predict.py
@@ -5,10 +5,8 @@ import numpy
 from music21 import instrument, note, stream, chord
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM, BatchNormalization as BatchNorm, Activation
-
 def generate():
     # Load the trained models for piano and violin
-    # Name of hdf5 (Piano,Violon)
     piano_model = load_model('weights-improvement-piano-01.hdf5')
     violin_model = load_model('weights-improvement-violin-01.hdf5')
 
@@ -18,33 +16,25 @@ def generate():
     with open('data/violin_notes', 'rb') as filepath:
         violin_notes = pickle.load(filepath)
 
-    # Get all pitch names for piano and violin
     piano_pitchnames = sorted(set(item for item in piano_notes))
     violin_pitchnames = sorted(set(item for item in violin_notes))
 
-    # Get the number of unique pitches for piano and violin
     piano_vocab = len(set(piano_notes))
     violin_vocab = len(set(violin_notes))
 
-    # Prepare input sequences for piano and violin
     piano_network_input, piano_normalized_input = prepare_sequences(piano_notes, piano_pitchnames, piano_vocab)
     violin_network_input, violin_normalized_input = prepare_sequences(violin_notes, violin_pitchnames, violin_vocab)
-
     # Generate notes for piano and violin
     piano_prediction_output = generate_notes(piano_model, piano_network_input, piano_pitchnames, piano_vocab)
     violin_prediction_output = generate_notes(violin_model, violin_network_input, violin_pitchnames, violin_vocab)
-
     # Create a stream with piano and violin notes
     midi_stream = create_midi(piano_prediction_output, violin_prediction_output)
-
     # Write the MIDI file
     midi_stream.write('midi', fp='output.mid')
-
 def prepare_sequences(notes, pitchnames, n_vocab):
     """ Prepare the sequences used by the Neural Network """
     # Map between notes and integers and back
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-
     sequence_length = 100
     network_input = []
     normalized_input = []
@@ -53,16 +43,12 @@ def prepare_sequences(notes, pitchnames, n_vocab):
         sequence_out = notes[i + sequence_length]
         network_input.append([note_to_int[char] for char in sequence_in])
         normalized_input.append(note_to_int[sequence_out])
-
     n_patterns = len(network_input)
-
     # Reshape the input into a format compatible with LSTM layers
     normalized_input = numpy.reshape(normalized_input, (n_patterns, 1))
     # Normalize input
     normalized_input = normalized_input / float(n_vocab)
-
     return (network_input, normalized_input)
-
 def load_model(model_path):
     """ Load the trained model """
     model = Sequential()
@@ -79,46 +65,37 @@ def load_model(model_path):
     model.add(Activation('relu'))
     model.add(BatchNorm())
     model.add(Dropout(0.3))
-    model.add(Dense(n_vocab))
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-
     # Load the weights
     model.load_weights(model_path)
-
     return model
-
 def generate_notes(model, network_input, pitchnames, n_vocab):
     """ Generate notes from the neural network based on a sequence of notes """
     # Pick a random sequence from the input as a starting point for the prediction
     start = numpy.random.randint(0, len(network_input)-1)
-
     int_to_note = dict((number, note) for number, note in enumerate(pitchnames))
-
     pattern = network_input[start]
     prediction_output = []
-
     # Generate notes
     for _ in range(500):
         prediction_input = numpy.reshape(pattern, (1, len(pattern), 1))
         prediction_input = prediction_input / float(n_vocab)
-
         prediction = model.predict(prediction_input, verbose=0)
-
         index = numpy.argmax(prediction)
         result = int_to_note[index]
         prediction_output.append(result)
-
         pattern.append(index)
         pattern = pattern[1:]
-
     return prediction_output
-
 def create_midi(piano_notes, violin_notes):
     """ Create a MIDI stream with piano and violin notes """
     offset = 0
     output_notes = []
-
     # Create piano notes
     for pattern in piano_notes:
         # Pattern is a chord
@@ -138,10 +115,8 @@ def create_midi(piano_notes, violin_notes):
             new_note.offset = offset
             new_note.storedInstrument = instrument.Piano()
             output_notes.append(new_note)
-
         # Increase offset for piano notes
         offset += 0.5
-
     # Create violin notes
     for pattern in violin_notes:
         # Pattern is a chord
@@ -161,14 +136,10 @@ def create_midi(piano_notes, violin_notes):
             new_note.offset = offset
             new_note.storedInstrument = instrument.Violin()
             output_notes.append(new_note)
-
         # Increase offset for violin notes
         offset += 0.5
-
     # Create a MIDI stream
     midi_stream = stream.Stream(output_notes)
-
     return midi_stream
-
 if __name__ == '__main__':
     generate()
